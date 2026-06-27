222b4138e2ce18eeff1a9912600e4138ce2fec51
rehan07-ops <joshfogi5005@gmail.com>
2026-06-27 08:50:21 +0530

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function startChat() {
+    document.getElementById('main-app').style.display = 'block';
+}
+
+function toggleMute() { alert("Mic toggled"); }
+function toggleCamera() { alert("Camera toggled"); }
+function applyFilter() { alert("Premium Filter Applied"); }
+function endCall() { alert("Call Ended"); }
